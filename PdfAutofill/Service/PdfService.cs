a4688c138d3b0abf33f5faee5986d0919b5c6f73
Kenneth Glawe <kenneth.glawe@gmail.com>
2017-09-08 14:53:24 +0200

    Add basic structure

---
+++ PdfAutofill/Service/PdfService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using iText.Kernel.Pdf;
+using PdfAutofill.Model;
+
+namespace PdfAutofill.Service
+{
+    public class PdfService
+    {
+        public PdfDocument FillPdf(PdfViewModel model)
+        {
+            // TODO
+            return null;
+        }
+    }
+}
