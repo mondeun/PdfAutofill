dfa6eeebb0017bd216b1ad03a697b3a973058f6c
Kenneth Glawe <kenneth.glawe@gmail.com>
2017-09-08 11:20:48 +0200

    Add viewmodel

---
+++ PdfAutofill/Model/PdfViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PdfAutofill.Model
+{
+    public class PdfViewModel
+    {
+        public string Url { get; set; }
+
+        public List<string> FieldsData { get; set; }
+    }
+}
