51c6d7ba79299bcaa99b743a9bcc3025979deae7
Kenneth Glawe <kenneth.glawe@gmail.com>
2017-09-11 12:03:49 +0200

    Test getting acro form fields

---
+++ PdfAutofill.Tests/PdfServiceTests.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using iText.Kernel.Pdf;
+using NUnit.Framework;
+using PdfAutofill.Model;
+using PdfAutofill.Service;
+
+namespace PdfAutofill.Tests
+{
+    [TestFixture]
+    public class PdfServiceTests
+    {
+        private PdfService _sut;
+        private PdfViewModel _pdf;
+        private PdfDocument _pdfDocument;
+
+        [SetUp]
+        public void Setup()
+        {
+            _sut = new PdfService();
+
+            _pdf = new PdfViewModel
+            {
+                Url = "https://www.lantmateriet.se/globalassets/fastigheter/andra-agare/blanketter-och-information/blanketter/anmalan_fornyelse_av_inskrivning.pdf",
+                FieldsData = new List<string>()
+            };
+
+            byte[] bytes;
+
+            using (var client = new WebClient())
+            {
+                bytes = client.DownloadData(new Uri(_pdf.Url));
+            }
+
+            var memStream = new MemoryStream(bytes);
+            var pdfReader = new PdfReader(memStream);
+
+            _pdfDocument = new PdfDocument(pdfReader);
+        }
+
+        [Test]
+        public void ReceivePdfWithForms_ReturnListOfFields()
+        {
+
+            var result = _sut.GetAcroFields(_pdfDocument);
+
+            foreach (var pair in result)
+            {
+                Console.WriteLine(pair.Key);
+            }
+
+            Assert.AreEqual(23, result.Count);
+            Assert.True(result.Count(x => x.Key == "Adress") == 1);
+        }
+    }
+}
--- PdfAutofill/Service/PdfService.cs
+++ PdfAutofill/Service/PdfService.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
+using iText.IO.Source;
 using iText.Kernel.Pdf;
+using iText.Forms;
+using iText.Forms.Fields;
+using iText.Layout;
 using PdfAutofill.Model;
 
 namespace PdfAutofill.Service
@@ -9,18 +15,37 @@ namespace PdfAutofill.Service
     {
         public string FillPdf(PdfViewModel model)
         {
-            byte[] bytes = { };
-            var base64 = string.Empty;
-            var pdfDoc = new PdfDocument(new PdfReader(string.Empty), new PdfWriter(string.Empty));
-            
+            byte[] bytes;
+            var base64 = string.Empty;            
 
             using (var client = new WebClient())
             {
                 bytes = client.DownloadData(new Uri(model.Url));
             }
 
+            var memStream = new MemoryStream(bytes);
+            var pdfReader = new PdfReader(memStream);
+            // var pdfWriter = new PdfWriter();
 
-            return base64;
+            var pdfDoc = new PdfDocument(pdfReader);
+
+            var fields = GetAcroFields(pdfDoc);
+
+            
+
+            return fields.ToString();
+        }
+
+        public List<KeyValuePair<string, PdfFormField>> GetAcroFields(PdfDocument doc)
+        {
+            var fields = new List<KeyValuePair<string, PdfFormField>>();
+
+            foreach (var field in PdfAcroForm.GetAcroForm(doc, false).GetFormFields())
+            {
+                fields.Add(field);
+            }
+
+            return fields;
         }
     }
 }
