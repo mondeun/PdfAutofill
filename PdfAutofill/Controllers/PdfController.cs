37851b0f214b999b5e23f85652c0e046d39abd6c
Kenneth Glawe <kenneth.glawe@gmail.com>
2017-10-10 12:09:49 +0200

    Revert to old input handling for pdf generator

--- PdfAutofill/Controllers/PdfController.cs
+++ PdfAutofill/Controllers/PdfController.cs
@@ -29,7 +29,7 @@ namespace PdfAutofill.Controllers
         }
 
         [HttpPost("new")]
-        public IActionResult PostHtml(string html)
+        public IActionResult PostHtml([FromBody]string html)
         {
             if (string.IsNullOrWhiteSpace(html))
             {
